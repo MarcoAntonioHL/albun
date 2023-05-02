5ba95728843950fa3337016869a0764f951cef9a
Marco <marco.hl9antonio@gmail.com>
2023-05-02 17:28:37 -0500

    card

--- src/components/CardCake.jsx
+++ src/components/CardCake.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { itemData } from '../data/cakes';
+import { DescriptionCake } from './DescriptionCake';
 
-export const CardCake = () => {
+
+
+export const CardCake = (cake) => {
+  const{img,title,bill,flavor,filling}=cake
+  
   return (
-    <div>CardCake</div>
+    <>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          image={img}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {bill}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          {/*<DescriptionCake></DescriptionCake>*/}
+        </CardActions>
+      </Card>
+    </>
   )
 }
--- src/pages/Cakes/index.jsx
+++ src/pages/Cakes/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { CardCake } from '@/src/components/CardCake'
 
 export const Cakes = () => {
   return (
     <>
         <h1>indexhomes</h1>
+        <CardCake/>
     </>
   )
 }
