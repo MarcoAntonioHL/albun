036cba893754625b0cd54f7d136af7509999d1fb
AnthonyCconislla <anthonyacconisllamedina@gmail.com>
2023-05-02 17:21:21 -0500

    appbar y backgruond

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,42 @@
+import { AppBar, Box, Container, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CakeIcon from '@mui/icons-material/Cake'
+
+const darkTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#92dad9',
+        },
+    },
+});
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <Box sx={{ display: 'flex' }}>
+
+                <ThemeProvider theme={darkTheme}>
+                    <AppBar component="nav" position="static">
+                        <Container maxWidth="lg">
+                            <Toolbar>
+                                <IconButton
+                                    size="large"
+                                    edge="start"
+                                    color="inherit"
+                                    aria-label="open drawer"
+                                    sx={{ mr: 2 }}
+                                >
+                                    <CakeIcon />
+                                </IconButton>
+                                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                    Tortas
+                                </Typography>
+                            </Toolbar>
+                        </Container>
+                    </AppBar>
+                </ThemeProvider>
+            </Box>
+        </>
+    )
 }
--- src/pages/Cakes/index.jsx
+++ src/pages/Cakes/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
 export const Cakes = () => {
+  const lista = [1, 2, 3, 4, 5, 6]
   return (
     <>
-        <h1>indexhomes</h1>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          alignContent="center"
+          wrap="wrap"
+        >
+          {lista &&
+            lista.map(cake => (
+              <Grid item xs={12} md={6} lg={4} key={cake}>
+                {cake}
+              </Grid>
+            ))
+          }
+        </Grid>
+      </Container>
     </>
   )
 }
