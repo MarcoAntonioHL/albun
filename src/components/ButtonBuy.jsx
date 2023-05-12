f816f70e166ec4f2abf01b9e38aeec0538c9ceb5
Marco <marco.hl9antonio@gmail.com>
2023-05-12 18:03:00 -0500

    build

--- src/components/ButtonBuy.jsx
+++ src/components/ButtonBuy.jsx
@@ -5,7 +5,7 @@ import CallIcon from '@mui/icons-material/Call'
 export const ButtonBuy = ({ cake }) => {
     const { title, bill } = cake
 
-    const link = `https://api.whatsapp.com/send?phone=51928751897&text=Hola Tortas Cusco!!%20quisiera%20comprar%20la%20torta%20${title}%20de%20${bill}`
+    const link = `https://api.whatsapp.com/send?phone=51928751897&text=Hola Tortas!!%20quisiera%20comprar%20la%20torta%20${title}%20de%20${bill}`
     return (
         <>
             <a href={link} target="_blank">
